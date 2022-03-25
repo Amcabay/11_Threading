efa361cfd4779998f3d1c93adac6714db89594a5
Amcabay <84550409+Amcabay@users.noreply.github.com>
2022-03-26 00:03:29 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+mutex mu;
+int count = 1;
+condition_variable cv;
+
+void PrintOdd()	// fungsi untuk mengeprint angka ganjil
+{
+	for (int count = 1; count <= 10; count++)	//looping angka ganjil
+	{
+		unique_lock<mutex> locker(mu);
+		// Kondisi untuk ganjil
+		if (count % 2 == 1)				
+		{
+			cout << count << endl;	
+
+			// fungsi untuk mendelay selama 1 detik 
+			this_thread::sleep_for(chrono::seconds(1));				
+		}
+	}
+	cout << "Ganjil Selesai " << endl;
+
+}
+
+// Fungsi untuk mengeprint angka genap
+void PrintEven()	
+{
+	//Looping angka genap
+	for (int count = 1; count <= 10; count++)	
+	{
+		//Mengkondisikan angka genap
+		if (count % 2 == 0)				
+		{
+			cout << count << endl;
+
+			// Fungsi untuk mendelay selama 2.5 detik
+			this_thread::sleep_for(chrono::milliseconds(2500));		
+		}
+	}
+	cout << "finished counting even number" << endl;
+}
+
+/*		//Nyoba --salah
+void PrintEven()
+{
+	for (; count < 10;)
+	{
+		std::unique_lock<std::mutex> locker(mu);
+		cond.wait(locker, []() { return (count % 2 == 0); });
+		std::cout << "From Even: " << count << std::endl;
+		count++;
+		locker.unlock();
+		cond.notify_all();
+	}
+}*/
+
+int main()
+{
+	// Memulai thread odd
+	thread thOdd(PrintOdd);
+
+	// Memulai thread even
+	thread thEven(PrintEven);
+
+	// Menunggu thread odd untuk selesai
+	thOdd.join();				
+	// Menunggu thread even untuk selesai
+	thEven.join();				
+
+
+	return 0;
+}
