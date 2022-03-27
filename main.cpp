cbedb0133e02a00c89e4a0aafcc6895de2df22a4
Amcabay <84550409+Amcabay@users.noreply.github.com>
2022-03-27 22:36:49 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,75 +3,68 @@
 #include <mutex>
 #include <condition_variable>
 
-using namespace std;
 
-mutex mu;
-int count = 1;
-condition_variable cv;
+std::mutex mu;          //Pendeklarasian Mutex
+std::condition_variable cond;          //
+int count;
 
-void PrintOdd()	// fungsi untuk mengeprint angka ganjil
+//Fungsi untuk mengeprint angka ganjil
+void PrintOdd()
 {
-	for (int count = 1; count <= 10; count++)	//looping angka ganjil
-	{
-		unique_lock<mutex> locker(mu);
-		// Kondisi untuk ganjil
-		if (count % 2 == 1)				
-		{
-			cout << count << endl;	
+    for (count = 0; count < 10; count++)
+    {
+        if (count % 2 == 1)
+        {
+            //Digunakan karena menggunakan condition_variable
+	    //Menunggu sampai main() untuk mengirim data
+            std::unique_lock<std::mutex> locker(mu);
+            cond.wait(locker, []() { return (count % 2 == 1); });
 
-			// fungsi untuk mendelay selama 1 detik 
-			this_thread::sleep_for(chrono::seconds(1));				
-		}
-	}
-	cout << "Ganjil Selesai " << endl;
+	    //Data terkirim, lock sudah dimiliki
+            std::cout << count << std::endl;
 
+	    //Fungsi untuk mendelay selama 1 detik
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+
+            locker.unlock();       //Unlock the Mutex
+            cond.notify_all();     //Notify PrintEven that is done
+        }
+    }
+    std::cout << "Angka Ganjil Selesai" << std::endl;
 }
 
-// Fungsi untuk mengeprint angka genap
-void PrintEven()	
-{
-	//Looping angka genap
-	for (int count = 1; count <= 10; count++)	
-	{
-		//Mengkondisikan angka genap
-		if (count % 2 == 0)				
-		{
-			cout << count << endl;
-
-			// Fungsi untuk mendelay selama 2.5 detik
-			this_thread::sleep_for(chrono::milliseconds(2500));		
-		}
-	}
-	cout << "Genap Selesai" << endl;
-}
-
-/*		//Nyoba --salah
+//Fungsi untuk mengeprint angka genap
 void PrintEven()
 {
-	for (; count < 10;)
-	{
-		std::unique_lock<std::mutex> locker(mu);
-		cond.wait(locker, []() { return (count % 2 == 0); });
-		std::cout << "From Even: " << count << std::endl;
-		count++;
-		locker.unlock();
-		cond.notify_all();
-	}
-}*/
+    for (count = 0; count < 10; count++)
+    {
+        if (count % 2 == 0)
+        {
+	    //menunggu sampai main() untuk mengirim data
+            std::unique_lock<std::mutex> locker(mu);
+            cond.wait(locker, []() { return (count % 2 == 0); });
+            std::cout << count << std::endl;
+
+	    //Fungsi untuk mendelay selama 2,5 detik / 2500 milliseconds
+            std::this_thread::sleep_for(std::chrono::milliseconds(2500));
+            locker.unlock();    //Unlock the Mutex
+            cond.notify_all();  //Notify PrintEven that is done
+        }
+    }
+    std::cout << "Angka genap Selesai";
+}
 
 int main()
 {
-	// Memulai thread odd
-	thread th1(PrintOdd);
+    //Spawn new thread that calls PrintOdd
+    std::thread t1(PrintOdd);
+		
+    //Spawn new thread that calls PrintEven
+    std::thread t2(PrintEven);
 
-	// Memulai thread even
-	thread th2(PrintEven);
+    //Synchronize threads
+    t1.join();          //Pauses until first finishes
+    t2.join();          //Pauses until first finishes
 
-	// Menunggu thread odd untuk selesai
-	th1.join();				
-	// Menunggu thread even untuk selesai
-	th2.join();				
-
-
-	return 0;
+    return 0;
 }
